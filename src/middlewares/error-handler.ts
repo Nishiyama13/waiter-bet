d5896adb1c7fbe3f52f4c2527e4dac19d20d4284
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-10-01 16:11:45 -0300

    chore: Update handleApplicationErrors middleware - Updated the handle Application Errors middleware to handle NotFoundError providing appropriate HTTP status codes and error messages

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -9,7 +9,11 @@ export type ApplicationError = {
 export function handleApplicationErrors(err: ApplicationError | Error, _req: Request, res: Response, next: NextFunction) {
     console.log(err);
 
-    //Enter possible errors
+    if (err.name === "NotFoundError") {
+        return res.status(httpStatus.NOT_FOUND).send({
+            message: err.message,
+        });
+    }
 
     console.log(err.name);
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
