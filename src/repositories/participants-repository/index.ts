45211a40a17367ee2f2bcb513624c3883ce4c3fc
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-10-02 13:54:09 -0300

    feat: Add unique name validation for participant creation. Implemented a check to ensure that participant names are unique before creating a new participant. If a participant with the same name already exists, a 409 Conflict error will be returned. - Added validateUniqueName function to participants service. - Created findParticipantByName function in participants repository. - Integrated name uniqueness check in createParticipant.

--- src/repositories/participants-repository/index.ts
+++ src/repositories/participants-repository/index.ts
@@ -1,13 +1,23 @@
 import { prisma } from 'config';
 import { CreateParticipantInput } from '../../protocols';
+
 async function create(data: CreateParticipantInput) {
     return prisma.participant.create({
         data,
     });    
 }
 
+async function findParticipantByName(name: string) {
+    return prisma.participant.findFirst({
+        where: {
+            name,
+        }
+    })
+}
+
 const participantsRepository = {
     create,
+    findParticipantByName,
 }
 
 export default participantsRepository;
--- src/services/participants-service/index.ts
+++ src/services/participants-service/index.ts
@@ -1,10 +1,18 @@
 import { Participant } from "@prisma/client";
 import { CreateParticipantInput } from "../../protocols";
 import participantsRepository from "../../repositories/participants-repository";
+import { duplicateParticipantError } from "../../errors";
+
+async function  validateUniqueName(name:string) {
+    const participantWithSameName = await participantsRepository.findParticipantByName(name);
+    if (participantWithSameName) {
+        throw duplicateParticipantError();
+    }
+}
 
 async function createParticipant({ name, balance }: CreateParticipantInput): Promise <Participant> {
 
-    //await validateUniqueName(name);
+    await validateUniqueName(name);
 
     const participant = await participantsRepository.create({
         name,
