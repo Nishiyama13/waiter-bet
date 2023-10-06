3cd22c3f8b579ead03ae8b861b72975ff54804a0
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-10-06 20:48:53 -0300

    feat(participants): apply ID validation to 'getParticipantById' route

--- src/routers/participants-router.ts
+++ src/routers/participants-router.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
-import { validateBody } from '../middlewares';
+import { validateBody, validateParams } from '../middlewares';
 import { createParticipantSchema } from '../schemas/participant-schema';
 import { createParticipant, getParticipantById, getParticipants } from '../controllers/participants-controller';
+import { idSchema } from '../schemas/id-schema';
 
 const participantsRouter = Router();
 
 participantsRouter
     .post('/', validateBody(createParticipantSchema), createParticipant)
     .get('/', getParticipants)
-    .get('/:id', getParticipantById);
+    .get('/:id', validateParams(idSchema), getParticipantById);
 
 export { participantsRouter };
