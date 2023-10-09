221391972c6544f9ba70f6868b11b117d04eb323
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-10-09 13:10:20 -0300

    feat: Updated environment configuration and command adjustments

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,8 +5,10 @@ export function loadEnv() {
     const path = process.env.NODE_ENV === 'test' 
         ? '.env.test' 
         : process.env.NODE_ENV === 'development' 
-        ? '.env.development' 
-        : '.env.production';
+        ? '.env.development'
+        : process.env.NODE_ENV === 'production' 
+        ? '.env.production'  
+        : '.env';
     
     const currentEnvs = dotenv.config({ path });
     dotenvExpand.expand(currentEnvs);
