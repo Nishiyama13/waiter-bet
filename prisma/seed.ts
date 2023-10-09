4fa6a4fac8bba10fad495575a96f572f5a9e66d6
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-10-09 07:12:51 -0300

    feat: Add Initial Participant Seed - Introduces a seed script to populate the database with an initial participant representing the Waiter bet account. if none exists. This ensures the presence of a default participant for the application.

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client' 
+
+const prisma = new PrismaClient();
+
+async function main() {
+    let participant = await prisma.participant.findFirst();
+    if (!participant) {
+        participant = await prisma.participant.create({
+            data: {
+                name: "Weiter Bet",
+                balance: 0,
+            },
+        });
+    }
+
+    console.log( {participant });    
+}
+
+main()
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
